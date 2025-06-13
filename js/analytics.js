9b4769f4345b6e71901133b9ae644d05cf499c23
Point Final Developer <point.final.project@gmail.com>
2025-06-13 14:57:17 +0700

    Enable Vercel Analytics

---
+++ js/analytics.js
@@ -0,0 +1,3 @@
+window.va = window.va || function () { (window.va.q = window.va.q || []).push(arguments); };
+window.va('create', 'oai_mC98vUpxw3sJsjF1o2f24Sxg'); // Replace with your data-id
+window.va('send', 'pageview'); 
